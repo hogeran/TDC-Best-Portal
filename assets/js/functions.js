74c27aa4e8aa2d809ffc4ef233dd1a38fe9967f5
Mikael Holgersson <mikael.holgersson@tdc.se>
2015-09-09 14:33:12 +0200

    lagt till js till top-nav

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
 
-  // Get started!
-
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
 });
+
